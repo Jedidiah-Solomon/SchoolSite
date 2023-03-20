a33f44f6a524b1ecd2ec1069e9af718bd50cb858
Jedidiah Solomon <jedybrown9821@gmail.com>
2023-03-20 17:16:51 +0100

    Uploade files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -104,27 +104,3 @@
 	})()
 
 })(jQuery);
-
-
-// -----------------------------------Back-to-top-button-----------------------------------//
-// Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-	mybutton.style.display = "block";
-  } else {
-	mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", function() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -6,6 +6,12 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
+ Version: 1.8.0
+  Author: Ken Wheeler
+ Website: http://kenwheeler.github.io
+    Docs: http://kenwheeler.github.io/slick
+    Repo: http://github.com/kenwheeler/slick
+  Issues: http://github.com/kenwheeler/slick/issues
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
