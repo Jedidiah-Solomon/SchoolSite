de3389201ef4d5c25d87a5afbbcda1b383f75a6c
Jedidiah Solomon <jedybrown9821@gmail.com>
2023-03-23 00:50:11 +0100

    CSS Back-to-top Added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -105,4 +105,27 @@
 
 })(jQuery);
 
-// Back-to-top
+//------------------- Back-to-top------------------------//
+// Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 150px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } 
+  else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+
+//
